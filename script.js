f3106bbbeeb29bf0ce0d912e8d710d462f450048
RichaLimbachiya <richalimbachiya2004@gmail.com>
2025-05-13 14:04:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-function indexOfIgnoreCase(str, subStr) {
-  if (!str || !subStr) {
-    return -1;
+function convertToRoman(num) {
+  const romanMap = [
+    ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1]
+  ];
+
+  let result = "";
+
+  for (let [symbol, value] of romanMap) {
+    while (num >= value) {
+      result += symbol;
+      num -= value;
+    }
   }
-  const lowerStr = str.toLowerCase();
-  const lowerSubStr = subStr.toLowerCase();
-  return lowerStr.indexOf(lowerSubStr);
+
+  return result;
 }
+
+console.log(convertToRoman(36)); // Output: XXXVI
+module.exports = convertToRoman;
